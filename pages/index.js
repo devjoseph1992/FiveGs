839adf597741ab6cf03785d98902b6f4be5c70bc
John Joseph Florina <devjosephflorina@gmail.com>
2021-02-24 08:42:52 +0800

    first commit and add mea tags index

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+     <Head>
+        <title>FiveGiftStore</title>
+        <meta name='description' content='Want cheap and quality clothing? FiveGiftStore finding you quality and low budget clothing just a click away'/>
+        <meta name='keywords' content='CHEAP, BRANDED, TOPS, SHORTS, PANTS, SHOES, SHIRT'/> 
+        <meta name='author' content='freelancer seller'/>
+      </Head> 
+    </>
+  )
+}
